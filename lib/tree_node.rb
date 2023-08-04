0aa589dcbd425cdba895765d0031481e96725c5d
Andoniank <andoniank@gmail.com>
2023-08-04 14:49:08 -0700

    Initialized instances and implemented parent= method

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,5 +1,29 @@
 class PolyTreeNode
-  def initialize
+    attr_reader :value, :parent, :children
+  def initialize(value)
+    @value = value
     @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    # p self
+    # p node
+   
+
+    if @parent
+        i = 0
+        @parent.children.each_with_index { |child, j| i == j if child == node } 
+        @parent.children.delete_at(i)
+    end
+    
+
+    @parent = node
+    
+    if !self.parent.nil?
+        if !node.children.include?(self) 
+            node.children << self 
+        end
+    end
   end
 end
